6bf54ba21e468833a47f15508a3ad5803949719f
CyberBitz <jenkinsta@gmail.com>
2025-12-23 12:34:49 -0600

    vite force dist and not public

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { resolve } from "node:path";
 
 export default defineConfig({
   build: {
+    outDir: "dist",
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
