fe6bcdc93dd9501619518b444b2aae05510d1da6
CyberBitz <jenkinsta@gmail.com>
2025-12-23 12:45:46 -0600

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from "vite";
-import { resolve } from "path";
+import { resolve } from "node:path";
 
 export default defineConfig({
   build: {
-    outDir: "dist",
     rollupOptions: {
-    }
-  }
+      input: {
+        main: resolve(__dirname, "index.html"),
+        cricket: resolve(__dirname, "cricket.html"),
+        frog: resolve(__dirname, "frog.html"),
+        smoke: resolve(__dirname, "smoke.html"),
+      },
+    },
+  },
 });
